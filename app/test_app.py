29e417096a7f4830b5e5271739af958247dd554b
Archit Patel <91666443+archittechienutz@users.noreply.github.com>
2025-06-01 18:53:47 +0000

    Complete CI/CD assignment1

---
+++ app/test_app.py
@@ -0,0 +1,22 @@
+import unittest
+from app.app import app
+
+class FlaskAppTests(unittest.TestCase):
+    def setUp(self):
+        app.testing = True
+        self.client = app.test_client()
+
+    def test_home_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_content(self):
+        response = self.client.get('/')
+        self.assertEqual(response.data.decode(), "Hello from Flask!")
+
+    def test_invalid_route(self):
+        response = self.client.get('/invalid')
+        self.assertEqual(response.status_code, 404)
+
+if __name__ == '__main__':
+    unittest.main()
